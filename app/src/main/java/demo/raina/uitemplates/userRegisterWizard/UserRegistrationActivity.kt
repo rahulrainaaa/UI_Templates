6a6bd53c4d3d9fc1b3ea6c745e69b447c121df4f
rahul.raina <rahul.raina@ril.com>
2021-04-11 05:28:41 +0530

    Initial Commit.

--- app/src/main/java/demo/raina/uitemplates/userRegisterWizard/UserRegistrationActivity.kt
+++ app/src/main/java/demo/raina/uitemplates/userRegisterWizard/UserRegistrationActivity.kt
@@ -1,8 +1,11 @@
 package demo.raina.uitemplates.userRegisterWizard
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import demo.raina.uitemplates.R
 import demo.raina.uitemplates.databinding.ActivityUserRegistrationBinding
 
 /**
@@ -22,7 +25,35 @@ class UserRegistrationActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         userRegistrationVM = ViewModelProvider(this).get(UserRegistrationVM::class.java)
         binding.vm = userRegistrationVM
+        binding.includeLayout.btnFragmentNavigation.setOnClickListener { btnFragmentNavigationClick() }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener { controller, destination, arguments ->
+
+            when (destination.id) {
+                R.id.personal_detail_fragment -> {
+                    binding.includeLayout.btnFragmentNavigation.text = "Save Details"
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                R.id.user_id_proof_fragment -> {
+                    binding.includeLayout.btnFragmentNavigation.text = "Verify Details"
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                }
+            }
+        }
     }
 
+    private fun btnFragmentNavigationClick() {
 
+        findNavController(R.id.nav_host_fragment).navigate(R.id.personal_detail_to_id_proof_fragment)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                findNavController(R.id.nav_host_fragment).popBackStack()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
